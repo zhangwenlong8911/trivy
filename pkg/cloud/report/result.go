0de680aa1de3d0cb20060b4a421bf9d383298be9
Liam Galvin <liam.galvin@aquasec.com>
2022-07-05 13:50:46 +0100

    add output for results for single aws resource

--- pkg/cloud/report/result.go
+++ pkg/cloud/report/result.go
@@ -2,30 +2,45 @@ package report
 
 import (
 	"fmt"
+
+	renderer "github.com/aquasecurity/trivy/pkg/report"
+	"github.com/aquasecurity/trivy/pkg/types"
+
+	"golang.org/x/term"
 )
 
 func writeResultsForARN(report *Report, option Option) error {
-	//
-	//w, _, err := term.GetSize(0)
-	//if err != nil {
-	//	w = 80
-	//}
+
+	width, _, err := term.GetSize(0)
+	if err != nil {
+		width = 80
+	}
+	_ = width
+
+	w := option.Output
 
 	// render scan title
-	_, _ = fmt.Fprintf(option.Output, "\n\x1b[1m  '%s' (AWS Account %s)\x1b[0m\n", option.Service, report.AccountID)
+	_, _ = fmt.Fprintf(w, "\n\x1b[1mResults for '%s' (AWS Account %s)\x1b[0m\n\n", option.ARN, report.AccountID)
 
 	for _, result := range report.Results {
+		var filtered []types.DetectedMisconfiguration
 		for _, misconfiguration := range result.Misconfigurations {
-			if misconfiguration.CauseMetadata.Resource != option.ARN {
+			if option.ARN != "" && misconfiguration.CauseMetadata.Resource != option.ARN {
 				continue
 			}
-			fmt.Println(misconfiguration.ID + ": " + misconfiguration.CauseMetadata.Resource)
+			if option.Service != "" && misconfiguration.CauseMetadata.Service != option.Service {
+				continue
+			}
+			filtered = append(filtered, misconfiguration)
+		}
+		if len(filtered) > 0 {
+			_, _ = fmt.Fprint(w, renderer.NewMisconfigRenderer(result.Target, filtered, false, true).Render())
 		}
 	}
 
 	// render cache info
 	if option.FromCache {
-		_, _ = fmt.Fprintf(option.Output, "\n\x1b[34mThis scan report was loaded from cached results. If you'd like to run a fresh scan, use --update-cache.\x1b[0m\n")
+		_, _ = fmt.Fprintf(w, "\x1b[34mThis scan report was loaded from cached results. If you'd like to run a fresh scan, use --update-cache.\x1b[0m\n")
 	}
 
 	return nil
